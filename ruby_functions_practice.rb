625c2da02f811fd3c24d4213424b55b49c7bbaba
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-06-16 16:42:07 +0100

    added months

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,17 @@ end
 def add_string_as_number(str_1, str_2)
     return str_1.to_i + str_2.to_i
 end
+
+def number_to_full_month_name(month)
+    case month
+when 1
+    "January"
+when 3
+    "March"
+when 9
+    "September"
+else
+    "invalid"
+end
+end
+
